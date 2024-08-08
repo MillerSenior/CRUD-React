0ba79b7d14479a869697c849ec34795738faf9b8
MillerSenior <anthony.millersr82@gmail.com>
2024-08-08 14:26:20 -0500

    5001

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,7 @@ function Login() {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/login', {username, password});
+            const response = await axios.post('http://localhost:5001/api/login', {username, password});
             console.log('Login response:', response.data); // Log entire response data
 
             // Ensure userId is stored if present
